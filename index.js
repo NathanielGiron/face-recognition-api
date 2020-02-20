71c19b97c55612d62d08041a83e25346b224a178
Nathaniel Giron <nathaniel.giron@gmail.com>
2020-02-19 22:26:41 -0800

    code cleanup

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 
 app.post('/signin', (req, res) =>  {
   if (req.body.email === db.users[0].email && req.body.password === db.users[0].password) {
-    res.json('success');
+    res.json(db.users[0]);
   } else {
     res.status(400).json('error logging in');
   }
